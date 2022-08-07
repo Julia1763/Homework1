504f8b8f8e3e6019ff7d17136d67403dacea86d3
Julia <odintsova.julia@mail.ru>
2022-08-07 16:25:10 +0300

    Написали решение к 4й задаче

--- Examples002/Program.cs
+++ Examples002/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int FNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int SNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int TNumber = int.Parse(Console.ReadLine());
+if (FNumber > SNumber && FNumber > TNumber)
+{
+Console.WriteLine($"Максимальное число = {FNumber}"); 
+}
+
+else
+{
+    if (SNumber > FNumber && SNumber > TNumber)
+    {
+     Console.WriteLine($"Максимальное число = {SNumber}");    
+    }
+    else
+    {
+      Console.WriteLine($"Максимальное число = {TNumber}");  
+    }
+  
+}
+ 
