ed57fcbd73896bb6da84634328d5031cb165f3f4
Julia <odintsova.julia@mail.ru>
2022-08-07 16:01:41 +0300

    Задача 2 выполнена

--- Examples001/Program.cs
+++ Examples001/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int FNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int SNumber = int.Parse(Console.ReadLine());
+if (FNumber > SNumber)
+{
+Console.WriteLine($"Максимальное число = {FNumber}");
+Console.WriteLine($"Минимальное число = {SNumber}");  
+}
+
+else
+{
+  Console.WriteLine($"Максимальное число = {SNumber}");
+  Console.WriteLine($"Минимальное число = {FNumber}"); 
+}
+ 
