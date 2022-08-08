5583bdf1563154709f1151fc12f838f07a364ff8
Julia <odintsova.julia@mail.ru>
2022-08-08 14:37:56 +0300

    Написали решение к 6й задаче

--- Examples003/Program.cs
+++ Examples003/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int Number = int.Parse(Console.ReadLine());
+if (Number % 2 == 0)
+{
+  Console.WriteLine($"Число {Number} является четным.");
+}
+else
+{
+    Console.WriteLine($"Число {Number} не является четным.");
+}
